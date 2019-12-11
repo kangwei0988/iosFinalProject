130465e7c84e53f68e28879448ef094b922a370d
user21 <osbert5618841@gmail.com>
2019-12-11 10:53:48 +0800

    Initial Commit

---
+++ iostest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iostest
+//
+//  Created by User21 on 2019/12/11.
+//  Copyright © 2019 kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
